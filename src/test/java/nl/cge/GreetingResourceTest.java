61e90d57d4dd54cda907ce6c77592fc67e058356
chrisvandergeer <chrisvandergeer@gmail.com>
2025-06-13 20:59:43 +0200

    Add greeting POST endpoint with time-based message

--- src/test/java/nl/cge/GreetingResourceTest.java
+++ src/test/java/nl/cge/GreetingResourceTest.java
@@ -3,6 +3,8 @@ package nl.cge;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -17,4 +19,27 @@ class GreetingResourceTest {
              .body(is("Hello from Quarkus REST"));
     }
 
+    @Test
+    void testGreetingPost() {
+        String partOfDay = calculatePartOfDay();
+
+        given()
+            .contentType("application/json")
+            .body("{\"name\":\"Codex\"}")
+            .when().post("/hello")
+            .then()
+                .statusCode(200)
+                .body("message", is("Hello Codex, een hele goede " + partOfDay));
+    }
+
+    private String calculatePartOfDay() {
+        int hour = LocalTime.now().getHour();
+        if (hour < 12) {
+            return "ochtend";
+        }
+        if (hour < 18) {
+            return "middag";
+        }
+        return "avond";
+    }
 }
